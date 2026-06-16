d85cfbdf626dbe7cb84133ddf63dd6c72519e033
kroulitsch <honzikkroulik@gmail.com>
2026-06-16 14:56:25 +0200

    Quick Start Example

--- main.c
+++ main.c
@@ -1,68 +1,18 @@
 #include "alterchord.h"
-#include <stdio.h>
+
+// Quick Start Example
 
 int main() {
     Tuning myTuning = createTuning(6, "FACGCG", 24);
+    Chord myChord = createChord("Gadd4");
 
-    Chord myChord = createChord("Fmaj7sus4");
+    // Generate the first shape (span = 4, capo = 0, maxFret = 12)
+    Shape s = getShape(myChord, myTuning, 4, 0, 12);
     
-    printf("Looking for shapes for a chord Fmaj7sus4 in tuning FACGCG\n");
-    printf("--------------------------------------------------\n");
+    // Iterate to the next available shape
+    Shape *next = getNextShape(&s, 4, 0, 12);
 
-    Shape s = getShape(myChord, myTuning, 2, 0, 12);
-    
-    if (s.frets == NULL) {
-        printf("No shape for this chord found.\n");
-        return 0;
-    }
-
-    Shape *current = &s;
-    int counter = 1;
-
-    while (current != NULL) {
-        printf("Shape %02d: ", counter++);
-        
-        for (int i = 0; i < current->tuning.stringCount; i++) {
-            if (current->frets[i] == MUTED) {
-                printf("X ");
-            } else {
-                printf("%d ", current->frets[i]);
-            }
-        }
-        printf("\n");
-
-        current = getNextShape(current, 2, 0, 12);
-    }
-
-    printf("--------------------------------------------------\n");
-    printf("All possible combinations printed out.\n");
-
-    printf("\n\n");
-    Shape clickedShape;
-    clickedShape.tuning = myTuning;
-    clickedShape.frets = (int[]){2, MUTED, 2, 4, 0, 0};
-
-    Chord foundChords[MAX_RESULTS];
-
-    int count = analyzeShape(clickedShape, foundChords, MAX_RESULTS);
-
-    printf("Shape analysis: 2 X 2 4 0 0 in tuning FACGCG\n");
-    printf("Found options (%d):\n", count);
-    printf("----------------------------------------\n");
-
-    if(count == 0) {
-        printf("No option found.");
-    }
-
-    for (int i = 0; i < count; i++) {
-        char chordStr[MAX_RESULTS];
-        chordNameToStr(foundChords[i], chordStr);
-
-        printf("Option %d: %s\n", i + 1, chordStr);
-    }
-    printf("----------------------------------------\n");
-
-    // 6. Úklid paměti
+    // Memory cleanup
     freeShapesList(&s);
     freeTuning(myTuning);
     return 0;
